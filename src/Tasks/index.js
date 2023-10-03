975467edcb3c967728ac0e5c4d5ea62e3f907b89
Szymon Rutkowski <Szymon.rutkowski1@gmail.com>
2023-10-03 19:32:49 +0200

    toggle task done interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,4 +1,4 @@
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <ul className="tasks">
     {tasks.map((task) => (
       <li
@@ -7,8 +7,10 @@ const Tasks = ({ tasks, hideDone, removeTask }) => (
           task.done && hideDone ? "tasks__item--hidden" : ""
         }`}
       >
-        <button className="tasks__button tasks__button--toggleDone">
-          {" "}
+        <button
+          className="tasks__button tasks__button--toggleDone"
+          onClick={() => toggleTaskDone(task.id)}
+        >
           {task.done ? "✔️" : ""}{" "}
         </button>
         <span
